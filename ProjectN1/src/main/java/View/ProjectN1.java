1d2116cc2d6d0b19be7085376c4c7236991eb396
MatusSM21 <cristophermatus9@gmail.com>
2023-10-08 14:02:03 -0600

    ProjectN1

---
+++ ProjectN1/src/main/java/View/ProjectN1.java
@@ -0,0 +1,12 @@
+package View;
+
+/**
+ *
+ * @author Cristopher Matus
+ */
+public class ProjectN1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
