4e3ab66901bbc61de5d2f0f5091721f01ba562c9
DeivisJm <deivisjm1205@gmail.com>
2023-10-09 08:51:03 -0600

    Creaciones

---
+++ ProjectIII/src/Model/ProjectIII.java
@@ -0,0 +1,21 @@
+package Model;
+
+import View.*;
+
+/**
+ *
+ * @author deivi
+ */
+public class ProjectIII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login l = new Login();
+
+        l.setVisible(true);
+
+    }
+
+}
