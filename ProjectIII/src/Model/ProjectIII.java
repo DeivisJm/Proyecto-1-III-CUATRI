1ec031ac0444702ea4bbef6366ee6f9d3e11fe4d
2123i3fabri <reyesufabricio@gmail.com>
2023-10-10 20:50:42 -0600

    Clase DBConnection y conexion a la base de datos

--- ProjectIII/src/Model/ProjectIII.java
+++ ProjectIII/src/Model/ProjectIII.java
@@ -1,7 +1,7 @@
 package Model;
 
 import View.*;
-
+import model.DBConnection;
 /**
  *
  * @author deivi
@@ -12,9 +12,12 @@ public class ProjectIII {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.disconnect();
+        
         Login l = new Login();
-
         l.setVisible(true);
+        
 
     }
 
