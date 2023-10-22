a4aa47eb53607f171ff2ef33392c2534a5bfb195
Matus <cristophermatus9@gmail.com>
2023-10-22 14:24:42 -0600

    Document the Entity class in English

--- ProjectIII/src/Model/Entity.java
+++ ProjectIII/src/Model/Entity.java
@@ -1,11 +1,13 @@
-package Model;
+package Model;  // Declare the package where this class is located.
 
 /**
+ * A class representing an entity.
  *
- * @author Cristopher Matus
+ * @author Cristopher Matus  // Author's name
  */
 public class Entity {
 
+    // Private instance variables to store the attributes of the entity.
     private int id;
     private int legal_id;
     private String name;
@@ -14,9 +16,11 @@ public class Entity {
     private String address;
     private String description;
 
+    // Default constructor with no parameters.
     public Entity() {
     }
 
+    // Constructor with parameters to initialize all attributes.
     public Entity(int id, int legal_id, String name, String email, int celphone, String address, String description) {
         this.id = id;
         this.legal_id = legal_id;
@@ -27,6 +31,7 @@ public class Entity {
         this.description = description;
     }
 
+    // Constructor without an 'id' parameter, used when 'id' is not known initially.
     public Entity(int legal_id, String name, String email, int celphone, String address, String description) {
         this.legal_id = legal_id;
         this.name = name;
@@ -36,100 +41,72 @@ public class Entity {
         this.description = description;
     }
 
-    /**
-     * @return the id
-     */
+    // Getter method for 'id'.
     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
+    // Setter method for 'id'.
     public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the legal_id
-     */
+    // Getter method for 'legal_id'.
     public int getLegal_id() {
         return legal_id;
     }
 
-    /**
-     * @param legal_id the legal_id to set
-     */
+    // Setter method for 'legal_id'.
     public void setLegal_id(int legal_id) {
         this.legal_id = legal_id;
     }
 
-    /**
-     * @return the name
-     */
+    // Getter method for 'name'.
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name the name to set
-     */
+    // Setter method for 'name'.
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return the email
-     */
+    // Getter method for 'email'.
     public String getEmail() {
         return email;
     }
 
-    /**
-     * @param email the email to set
-     */
+    // Setter method for 'email'.
     public void setEmail(String email) {
         this.email = email;
     }
 
-    /**
-     * @return the celphone
-     */
+    // Getter method for 'celphone'.
     public int getCelphone() {
         return celphone;
     }
 
-    /**
-     * @param celphone the celphone to set
-     */
+    // Setter method for 'celphone'.
     public void setCelphone(int celphone) {
         this.celphone = celphone;
     }
 
-    /**
-     * @return the address
-     */
+    // Getter method for 'address'.
     public String getAddress() {
         return address;
     }
 
-    /**
-     * @param address the address to set
-     */
+    // Setter method for 'address'.
     public void setAddress(String address) {
         this.address = address;
     }
 
-    /**
-     * @return the description
-     */
+    // Getter method for 'description'.
     public String getDescription() {
         return description;
     }
 
-    /**
-     * @param description the description to set
-     */
+    // Setter method for 'description'.
     public void setDescription(String description) {
         this.description = description;
     }
